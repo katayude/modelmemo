56ac850c734f2666453fc24ca396a69ea3080114
katayude <xXgucchiXx@gmail.com>
2023-12-15 16:30:48 +0900

    ルートディレクトリを作成

---
+++ src/app/example/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TutorialPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Tutorial</h1>
+            <p>Welcome to the tutorial page!</p>
+        </div>
+    );
+};
+
+export default TutorialPage;
