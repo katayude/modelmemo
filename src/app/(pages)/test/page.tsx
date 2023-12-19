518c30d7676782a63d2d3a7a0bc89c5ca1507e81
ryogoooo <iidaryogo0526@gmail.com>
2023-12-19 17:41:52 +0900

    navigation

---
+++ src/app/(pages)/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidemenue from '@/app/components/Sidemenue';
+
+const Page: React.FC = () => {
+    return (
+        <div>
+            <Sidemenue />
+        </div>
+    );
+};
+
+export default Page;
--- src/app/components/Sidemenue.tsx
+++ src/app/components/Sidemenue.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Sidemenue: React.FC = () => {
     return (
         <div>
-            {/* Sidemenueのコンテンツをここに追加 */}
+            <nav>
+                <ul>
+                    <li id="01"></li>
+                    <li id="02"><span>aaa</span><span>今いる場所</span></li>
+                    <li id="03"><a href="images">画像一覧</a></li>
+                    <li id="03"><a href="3dmodels">３Dモデル</a></li>
+                </ul>
+            </nav>
         </div>
     );
 };
