f1b023fbda604ea9f6456194dd35e91e711d4ff8
katayude <xXgucchiXx@gmail.com>
2023-12-20 23:44:52 +0900

    mainコンポーネント少し触りました

--- src/app/components/Mainmenue.tsx
+++ src/app/components/Mainmenue.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Genbalist from '@/app/components/Genbalist';
 
 const Mainmenue: React.FC = () => {
     return (
         <div>
-            {/* ここにコンポーネントの内容を追加してください */}
+
+            <Image src="/images/plus.png" alt="logo" width={100} height={100} />
+
+            <a href="javascript:history.back();">
+                <Image src="/images/return.png" alt="logo" width="100" height="100" />
+            </a>
+
+            <Genbalist
+                building='理系図書館'
+                location='2F'
+                personInCharge='飯田' />
+
         </div>
     );
 };
