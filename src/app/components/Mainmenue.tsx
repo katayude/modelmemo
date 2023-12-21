66537f87df49bc1583fec4c22a9525b5514a67e6
katayude <xXgucchiXx@gmail.com>
2023-12-21 12:03:03 +0900

    roomlistを作成しました

--- src/app/components/Mainmenue.tsx
+++ src/app/components/Mainmenue.tsx
@@ -7,19 +7,35 @@ const Mainmenue: React.FC = () => {
     return (
         <div>
 
-            <Image src="/images/plus.png" alt="logo" width={100} height={100} />
-
-            <a href="javascript:history.back();">
-                <Image src="/images/return.png" alt="logo" width="100" height="100" />
-            </a>
-
             <Genbalist
                 building='理系図書館'
                 location='2F'
                 personInCharge='飯田' />
 
+            <Genbalist
+                building='理系図書館'
+                location='1F'
+                personInCharge='山口' />
+            <Genbalist
+                building='理系図書館'
+                location='3F'
+                personInCharge='鵜飼' />
+
+            <div style={aiconstyle}>
+                <Image src="/images/plus.png" alt="logo" width={100} height={100} />
+
+                <a href="javascript:history.back();">
+                    <Image src="/images/return.png" alt="logo" width="100" height="100" />
+                </a>
+            </div>
+
         </div>
     );
 };
 
+const aiconstyle = {
+    display: 'flex',
+    justifyContent: 'space-between'
+}
 export default Mainmenue;
+
