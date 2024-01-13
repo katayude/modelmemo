2a7d5c528916a886de1d5c831c0f54c107a5a528
katayude <xXgucchiXx@gmail.com>
2024-01-14 04:27:56 +0900

    なんか消えてたので修正

--- src/app/(pages)/product_3Dview/[id]/page.tsx
+++ src/app/(pages)/product_3Dview/[id]/page.tsx
@@ -23,7 +23,7 @@ const App: React.FC<ThreeDviewProps> = ({ params }) => {
                 {/* 画像を矢印に差し替える */}
                 <div className={styles.arrows}>
                     <img className={styles.img} src="/images/return.png" alt="*" />
-                    <Link className={styles.link} href={`/product_3Dedit`} passHref>
+                    <Link className={styles.link} href={`/product_3Dedit/${Number(params.id)}`} passHref>
                         <img className={styles.img} src="/images/edit.png" alt="*" />
                     </Link>
                 </div>
